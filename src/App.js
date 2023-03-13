d4e95f159d3beed2fd247769ca620d32cbef3472
shaikat17 <shaikatpal56@gmail.com>
2023-03-14 00:17:40 +0600

    dark mode toggle button is working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Article from './Article';
 import data from './data'
 
 
 function App() {
+  const [theme, setTheme] = useState("light-theme");
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  },[theme])
+
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>overreacted</h1>
-          <button className="btn">toggle</button>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
         </div>
       </nav>
       <section className="articles">
-        {data.map( item => {
-          return <Article key={item.id} {...item} />
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
         })}
       </section>
     </main>
